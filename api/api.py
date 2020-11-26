8191004b7c9ea81da1ba8913e28a5ddbcd3f2e26
lxqiang95 <lxq13925731395@163.com>
2020-11-26 13:07:58 +0800

    接口测试

---
+++ api/api.py
@@ -0,0 +1,6 @@
+# _*_ coding: UTF-8 _*_
+# @Time     : 2020/11/26 13:04
+# @Author   : LiuXiaoQiang
+# @Site     : http:www.cdtest.cn/
+# @File     : api.py
+# @Software : PyCharm
---
+++ testcase/__init__.py
@@ -0,0 +1,6 @@
+# _*_ coding: UTF-8 _*_
+# @Time     : 2020/11/26 13:03
+# @Author   : LiuXiaoQiang
+# @Site     : http:www.cdtest.cn/
+# @File     : __init__.py.py
+# @Software : PyCharm
